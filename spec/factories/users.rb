15852b0e8567396fa7a2e02be60186c449184931
Aleksandra <aleksandra.stolyar@gmail.com>
2018-04-11 16:40:00 +0300

    Init files

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    email "MyString"
+    password "MyString"
+    role "MyString"
+    full_name "MyString"
+    avatar "MyString"
+    position "MyString"
+  end
+end
