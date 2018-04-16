638632f531cca55d513cf50c1cbfc54baad464e5
Aleksandra <aleksandra.stolyar@gmail.com>
2018-04-16 18:21:18 +0300

    Add services

--- db/migrate/20180406123645_services.rb
+++ db/migrate/20180406123645_services.rb
@@ -1,4 +1,11 @@
 class Services < ActiveRecord::Migration[5.0]
   def change
+    create_table :services do |t|
+      t.string :name
+      t.string :logo
+      t.string :link
+
+      t.timestamps null: false
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 20180409171635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password"
